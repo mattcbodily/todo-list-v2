43701b51282045d24251f4d98cb3fe7c96745b62
mattcbodily <mattcbodily@gmail.com>
2020-08-25 14:22:58 -0600

    users can now change the progress of their tasks

--- src/Components/TaskDashboard/TaskDashboard.js
+++ src/Components/TaskDashboard/TaskDashboard.js
@@ -10,6 +10,7 @@ export default props => {
         const {user, userSetFn} = props;
 
         let task = {
+            id: user.tasks.length ? user.tasks[user.tasks.length - 1].id + 1 : 1,
             task: taskInput,
             progress: 'Not Started'
         }
@@ -22,16 +23,27 @@ export default props => {
         setTaskInput('');
     }
 
+    const updateProgress = (progress, id) => {
+        const {user, userSetFn} = props;
+
+        let userCopy = {...user},
+            task = userCopy.tasks.find(e => e.id === id);
+
+        task.progress = progress;
+        localStorage.setItem('userObj', JSON.stringify(userCopy));
+        userSetFn(userCopy);
+    }
+
     return (
         <section className='task-dashboard'>
             <input value={taskInput} placeholder='Enter Task' onChange={e => setTaskInput(e.target.value)}/>
             <button onClick={addTask}>Add</button>
             {props.user.tasks && taskView === 'current'
             ? props.user.tasks?.filter(task => task.progress === 'Not Started' || task.progress === 'In Progress').map((task, i) => (
-                <TaskDisplay key={i} task={task}/>
+                <TaskDisplay key={i} task={task} user={props.user} progressFn={updateProgress}/>
             ))
             : props.user.tasks?.filter(task => task.progress === 'Complete').map((task, i) => (
-                <TaskDisplay key={i} task={task}/>
+                <TaskDisplay key={i} task={task} user={props.user} progressFn={updateProgress}/>
             ))}
 
         </section>
--- src/Components/TaskDisplay/TaskDisplay.js
+++ src/Components/TaskDisplay/TaskDisplay.js
@@ -7,9 +7,9 @@ export default props => {
         <div className='task-display'>
             <p>{props.task.task}</p>
             <div className='task-progress'>
-                <div id='not-started' className={`progress-option ${props.task.progress === 'Not Started' ? 'active' : null}`}>Not Started</div>
-                <div id='in-progress' className={`progress-option ${props.task.progress === 'In Progress' ? 'active' : null}`}>In Progress</div>
-                <div id='complete' className={`progress-option ${props.task.progress === 'Complete' ? 'active' : null}`}>Complete</div>
+                <div className={`progress-option ${props.task.progress === 'Not Started' ? 'active' : null}`} onClick={() => props.progressFn('Not Started', props.task.id)}>Not Started</div>
+                <div id='in-progress' className={`progress-option ${props.task.progress === 'In Progress' ? 'active' : null}`} onClick={() => props.progressFn('In Progress', props.task.id)}>In Progress</div>
+                <div className={`progress-option ${props.task.progress === 'Complete' ? 'active' : null}`} onClick={() => props.progressFn('Complete', props.task.id)}>Complete</div>
             </div>
         </div>
     )
