aeda94fca42771b5f4ae7814f67362b0626ab136
mattcbodily <mattcbodily@gmail.com>
2020-08-25 15:13:41 -0600

    added styling changes to Header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 import './Header.scss';
 
 export default props => {
-    let [username, setUsername] = useState(props.user.username),
+    let [username, setUsername] = useState(props.user.username || ''),
         [edit, setEdit] = useState(false);
 
     const addUsername = () => {
@@ -24,9 +24,11 @@ export default props => {
                 </section>
             )
             : (
-                <section>
+                <section className='username-flex'>
                     <p>Welcome, {props.user.username}</p>
-                    <button onClick={() => setEdit(true)}>Change Username</button>
+                    <svg onClick={() => setEdit(true)} xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-edit-2">
+                        <path d="M17 3a2.828 2.828 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5L17 3z"></path>
+                    </svg>
                 </section>
             )}
         </header>
